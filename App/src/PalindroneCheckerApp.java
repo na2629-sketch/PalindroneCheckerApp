916da0037d1eb02d5b79611c6fdb8a07c73fde81
na2629-sketch <na2629@srmist.edu.in>
2026-02-26 10:20:54 +0530

    [Raj] Added USECASE RELATED RAJ

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,13 +1,13 @@
-//version 2.0
+//version 3.0
 //author Abc
-//useCase 2:Welcome page/UseCase
-public class PalindroneCheckerApp git {
+//useCase 3:Welcome page/UseCase
+public class PalindroneCheckerApp gitgit checkout maingit checkout main {
     public static void main(String[] args){
         int n=5;
         System.out.println("Welcome to palindrone checker App Management System:");
         System.out.println("Modify this logic to usecase 3");
         for (int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
+            for(int j=i;j<n;j++){
                 System.out.print("*");
             }
             System.out.println();
