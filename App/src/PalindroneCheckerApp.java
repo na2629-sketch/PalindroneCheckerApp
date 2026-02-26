4679378647fa65a21768c3a52696211babbd43ef
na2629-sketch <na2629@srmist.edu.in>
2026-02-26 09:17:23 +0530

    [Raj] Added usecaseRaj

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,8 @@
+//version 1.0
+//author Abc
+//useCase 1:Welcome page
+public class PalindroneCheckerApp git {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrone checker App Management System:");
+    }
+}
